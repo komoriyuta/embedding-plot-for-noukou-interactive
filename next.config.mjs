de9a7ef9cc1afdfb85cd5001880c19e8a849a7c9
komoriyuta <hamucha565@gmail.com>
2024-10-18 01:04:41 +0900

    standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     assetPrefix: '/noukou-demo/',
+    output: 'standalone',
     experimental: {
         serverActions: {
             bodySizeLimit: '10mb' // Set desired value here
