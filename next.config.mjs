1ed4b0832a4219ae310e50900457a69bb04dbb54
komoriyuta <hamucha565@gmail.com>
2024-10-18 00:59:24 +0900

    assetprefix?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '/noukou-demo',
+    assetPrefix: '/noukou-demo/',
     experimental: {
         serverActions: {
             bodySizeLimit: '10mb' // Set desired value here
