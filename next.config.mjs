2c026612a426d88ce82fd06eae11e8369af4fa18
komoriyuta <hamucha565@gmail.com>
2024-10-18 01:19:17 +0900

    main

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '/noukou-demo/',
+   // assetPrefix: '/noukou-demo/',
     output: 'standalone',
     experimental: {
         serverActions: {
--- src/components/embeddingPlot.tsx
+++ src/components/embeddingPlot.tsx
@@ -218,7 +218,7 @@ export default function TextEmbeddingVisualization() {
       };
   
       fetchEmbeddings();
-    }, [texts]);
+    }, []);
   
 
     return (
